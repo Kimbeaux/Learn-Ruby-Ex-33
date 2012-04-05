4b8aa85aa89dec889e45c39dfb71b53a8c6d8331
Kimbeaux <KimbeauxE@verizon.net>
2012-04-04 22:04:41 -0400

    ex33 w/ ec 1-4

--- ex33.rb
+++ ex33.rb
@@ -1,17 +1,22 @@
-i = 0
-numbers = []
+def build_array(size, incr)
+# builds an array of elements from 0 to (size-1) in increments of incr
+  i = 0
+  numbers = []
 
-while i < 6
-  puts "At the top i is #{i}."
-  numbers.push(i)
+  while i < size
+    puts "At the top i is #{i}."
+    numbers.push(i)
 
-  i = i + 1
-  puts "Numbers now: #{numbers}"
-  puts "At the bottom i is #{i}."
+    i = i + incr
+    puts "Numbers now: #{numbers}"
+    puts "At the bottom i is #{i}."
+  end
+
+  puts "The numbers: "
+
+  for num in numbers
+    puts num
+  end
 end
 
-puts "The numbers: "
-
-for num in numbers
-  puts num
-end
+build_array(100,10)
